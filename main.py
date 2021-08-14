50869dbaf2db967b0f46c1673418c2d25caf43d9
wsqhao <35697616+wsqhao@users.noreply.github.com>
2021-08-14 10:06:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests,time,re,json,random
 
 now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 headers = {
-        'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; MI 6 MIUI/20.6.18)'
+        'User_Agent':'Mozilla/5.0 (Linux; Android 10; BMH-AN10 Build/HUAWEIBMH-AN10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 JsSdk/2 NewsArticle/8.3.5 NetType/5g (NewsLite 8.3.5)'
         }
  
 #获取登录code
